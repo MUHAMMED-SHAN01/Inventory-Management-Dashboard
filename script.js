4c236d7dd11f975f5856a0631780cf5db6a62105
Muhammed Shan <muhammed.shan.tvm01@gmail.com>
2026-08-17 06:09:43 -0700

    from validate and make object with push array

--- script.js
+++ script.js
@@ -1,10 +1,50 @@
+// menu bar ........
+
 const menuBtn= document.querySelector("#menuBtn");
-console.log(menuBtn);
+// console.log(menuBtn);
 const sidebar=document.querySelector("#sidebar");
-console.log(sidebar);
-
-
+// console.log(sidebar);
 
 menuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("closed");
 })
+
+
+
+// from validation and add 
+
+products=[]; 
+const from=document.querySelector('#productFrom');
+const AddBtn=document.getElementById('addBtn');
+// console.log(AddBtn);
+const ProductName=document.getElementById('productName');
+// console.log(ProductName);
+const Categories=document.getElementById('Categories');
+// console.log(Categories);
+const Price=document.getElementById('Price');
+// console.log(Price);
+const Quantity=document.getElementById('Quantity');
+// console.log(Quantity);
+
+AddBtn.addEventListener("click", () => {
+
+    // create object
+   const product ={
+    name: ProductName.value,
+    category:Categories.value,
+    price: Price.value,
+    quantity: Quantity.value
+   };
+
+
+    // push array
+    
+    products.push(product);
+  
+   console.log(products);
+   from.reset();
+
+    
+});
+
+
