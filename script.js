ee5f6c163d4e2aba79dc15119e653c2abd20f9d9
Muhammed Shan <muhammed.shan.tvm01@gmail.com>
2026-08-19 06:36:05 -0700

    Add product page and delete option

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ const menuBtn= document.querySelector("#menuBtn");
 // console.log(menuBtn);
 const sidebar=document.querySelector("#sidebar");
 // console.log(sidebar);
-
+if(menuBtn && sidebar){
 menuBtn.addEventListener("click", () => {
     sidebar.classList.toggle("closed");
-})
+});
+}
 
 
+// Dashboard.Html
 
 // from validation and add 
 // retrive localstorage array 
 
 const products=JSON.parse(localStorage.getItem("inventoryData")) || []; 
-console.log(products);
+// console.log(products);
 
 const from=document.querySelector('#productFrom');
 const AddBtn=document.getElementById('addBtn');
@@ -30,7 +32,7 @@ const Quantity=document.getElementById('Quantity');
 // console.log(Quantity);
 
 
-
+if(AddBtn){
 AddBtn.addEventListener("click", () => {
 
     // check all inputs
@@ -51,24 +53,46 @@ AddBtn.addEventListener("click", () => {
     quantity: Quantity.value
    };
 
-
-    // push array
-    
+    // push array  
     products.push(product);
-
-
-    // store in localstorage
+  // store in localstorage
     localStorage.setItem("inventoryData",JSON.stringify(products));
-  
-   console.log(products);
-
-
+//    console.log(products);
    from.reset();
 
 }
-
-    
 });
+}
+
+
+
+// Products.html
+
+
+const productList=document.getElementById("productsList");
+
+if (productList){
+      
+    
+    products.forEach((product, index) => {
+
+        const card=document.createElement("div");
+
+        card.classList.add("product_cards");
+
+        card.innerHTML= ` <h3>${product.name}
+         </h3>
+        <p>Category: ${product.category}</p>
+        <p>Price: Rs${product.price}</p>
+        <p>Quantity:${product.quantity}</p> 
+        <button class="delBtn"data-index="${index}">Delete </button> `;
+
+
+        productList.appendChild(card);
+
+
+    });
+}
 
 
  
